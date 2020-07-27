4ec8d097f5c7f880215e8e6cd474c1df66fedab8
giladmp1978 <63838341+giladmp1978@users.noreply.github.com>
2020-07-27 16:05:20 +0300

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
-for i in {1..100}
-do
-echo "$i"
-sleep 6
-done
+#! /bin/bash
+echo "#! /bin/bash" > /tmp/newscrip.sh
+echo "for i in {1..100}" >> /tmp/newscrip.sh
+echo "do" >> /tmp/newscrip.sh
+echo "echo \"printing from new script for the \$i time \"" >> /tmp/newscrip.sh
+echo "sleep 3" >> /tmp/newscrip.sh
+echo "done" >> /tmp/newscrip.sh
+
+chmod +x /tmp/newscrip.sh
+/tmp/newscrip.sh &
