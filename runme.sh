acdd08a458fe293047c3bbaaa8325d1cd53cc31e
giladmp1978 <63838341+giladmp1978@users.noreply.github.com>
2021-10-18 20:21:42 +0300

    Update runme.sh
    added global input

--- runme.sh
+++ runme.sh
@@ -3,6 +3,7 @@ echo "#! /bin/bash" > /tmp/newscrip.sh
 echo "for i in {1..100}" >> /tmp/newscrip.sh
 echo "do" >> /tmp/newscrip.sh
 echo "echo \"printing from new script for the \$i time \" >> /tmp/scriptout.txt" >> /tmp/newscrip.sh
+echo "echo \"printing global input \$global  \" >> /tmp/scriptout.txt" >> /tmp/newscrip.sh
 echo "sleep 1" >> /tmp/newscrip.sh
 echo "done" >> /tmp/newscrip.sh
 
