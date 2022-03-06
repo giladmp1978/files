f1e978b12d46b5ab8af36ddede0f48c0594592e2
giladmp1978 <63838341+giladmp1978@users.noreply.github.com>
2022-03-06 09:59:57 +0200

    Update runme.sh
    add env variables print

--- runme.sh
+++ runme.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 echo "#! /bin/bash" > /tmp/newscrip.sh
+echo "printenv >> /tmp/scriptout.txt"
 echo "for i in {1..100}" >> /tmp/newscrip.sh
 echo "do" >> /tmp/newscrip.sh
 echo "echo \"printing from new script for the \$i time \" >> /tmp/scriptout.txt" >> /tmp/newscrip.sh
