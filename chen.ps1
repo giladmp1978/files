f36d5bb7c320d11abcbc5b06648ee4c06ea40310
giladmp1978 <63838341+giladmp1978@users.noreply.github.com>
2023-05-29 14:29:38 +0300

    Create chen.ps1

---
+++ chen.ps1
@@ -0,0 +1,15 @@
+$TokenSet = @{
+        U = [Char[]]'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'        
+    }
+
+$StringSet = Get-Random -Count 6 -InputObject $TokenSet.U
+$StringSet = 'DO-VM-' + $StringSet
+
+Write-Host $StringSet
+
+$StringSet = $StringSet -replace " ",""
+
+Write-Host $StringSet
+
+Rename-Computer -NewName  $StringSet
+Restart-Computer
