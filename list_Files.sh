1342780070adecf88f57df18f412c983ed0f3cb7
giladmp1978 <63838341+giladmp1978@users.noreply.github.com>
2023-11-21 10:02:49 +0200

    Update list_Files.sh

--- list_Files.sh
+++ list_Files.sh
@@ -3,11 +3,15 @@ echo "this is the correct file "
 echo "going down"
 date
 sleep 4
-date
 echo "Im back"
+date
 cd /
 pwd
 ls -la
 touch /tmp/myNewFile
 ls -la >> /tmp/myNewFile
 echo "listed files should be in /tmp/myNewFile"
+echo "*******************************************"
+echo "printing all environment variables"
+echo "*******************************************"
+printenv
